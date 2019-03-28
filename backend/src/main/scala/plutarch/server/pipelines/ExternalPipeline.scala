c11b07e272e371e03508c61deadfa85bec5872e6
rednikotin <nikotin@pythian.com>
2019-03-28 23:27:23 +0300

    testing

--- backend/src/main/scala/plutarch/server/pipelines/ExternalPipeline.scala
+++ backend/src/main/scala/plutarch/server/pipelines/ExternalPipeline.scala
@@ -21,17 +21,18 @@ class ExternalPipeline(
     scales = Seq(1, 5, 10, 30, 60, 300, 600, 1800, 3600, 3600 * 6, 3600 * 12, 3600 * 24),
     aggregations = Seq(Sum, Min, Max, Count, Avg,
       //PercentileCont(0.01),
-      PercentileCont(0.05),
+      //PercentileCont(0.05),
       //PercentileCont(0.10),
       //PercentileCont(0.25),
-      PercentileCont(0.50),
+      //PercentileCont(0.50),
       //PercentileCont(0.75),
       //PercentileCont(0.90),
-      PercentileCont(0.95),
+      //PercentileCont(0.95),
       //PercentileCont(0.99),
-      Stdev,
-      First,
-      Last),
+      //Stdev,
+      //First,
+      //Last
+    ),
     withTotal = false)
 
   val metric: Metric = metricManager.getOrCreate(conf)
